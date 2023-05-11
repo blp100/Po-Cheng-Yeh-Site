632bacf77bc715bb750f7d5af0c3b116e020804c
Brian <blp100x@hotmail.com>
2023-05-12 01:08:38 +0800

    fear: add personal info and paragraph animation

---
+++ components/paragraph.js
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+const Paragragh = styled.p`
+  text-glign: justify;
+  text-indent: 1em;
+`;
+export default Paragragh;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,15 @@
-import { Box, Container, Heading, useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Image,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Section from "../components/section";
+import Paragragh from "../components/paragraph";
+import NextLink from "next/link";
+import { CheckCircleIcon } from "@chakra-ui/icons";
 
 const Page = () => {
   return (
@@ -9,17 +20,51 @@ const Page = () => {
         p="6"
         mt="6"
         mb="6"
-        align="center"
+        alignItems="center"
       >
         Hello, I'm a developer live in Taiwan and Singapore. <br />
         60-30-10 Color Rule
       </Box>
-      <Box>
+      <Box display={{ md: "flex" }}>
         <Box flexGrow={1}>
-          <Heading>Po-Cheng Yeh</Heading>
+          <Heading as="h2" variant="page-title">
+            Po-Cheng Yeh
+          </Heading>{" "}
+          <p>Gamer, Boardgamer & Programmer</p>
         </Box>
-        <p>Gamer, Boardgamer & Programmer</p>
+
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="6.25rem"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/profile-1.jpg"
+            alt="Profile Image"
+          ></Image>
+        </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragragh>
+          I'm a Full-Stack programmer. I was a store manager. And love web
+          design and going back to the Internet again.{" "}
+        </Paragragh>
+        <Box alignItems="center" my={4}>
+          <Button rightIcon={<CheckCircleIcon />} colorScheme="teal">
+            My portfolio
+          </Button>
+        </Box>
+      </Section>
     </Container>
   );
 };
