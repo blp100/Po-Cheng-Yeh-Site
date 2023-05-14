da5c1ceb3b1271fc1f85f698973ad8d0fa1ba6df
Po-Cheng Yeh <blp100x@hotmail.com>
2023-05-14 15:34:56 +0800

    feat: add bio info

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import Section from "../components/section";
 import Paragragh from "../components/paragraph";
 import NextLink from "next/link";
 import { CheckCircleIcon } from "@chakra-ui/icons";
+import { BioSection, BioYear } from "../components/bio";
 
 const Page = () => {
   return (
@@ -20,10 +21,9 @@ const Page = () => {
         p="6"
         mt="6"
         mb="6"
-        alignItems="center"
+        textAlign="center"
       >
-        Hello, I'm a developer live in Taiwan and Singapore. <br />
-        60-30-10 Color Rule
+        Hello, I'm a developer live in Taiwan and Singapore.
       </Box>
       <Box display={{ md: "flex" }}>
         <Box flexGrow={1}>
@@ -46,7 +46,7 @@ const Page = () => {
             maxWidth="6.25rem"
             display="inline-block"
             borderRadius="full"
-            src="/images/profile-1.jpg"
+            src="/images/profile-2.jpg"
             alt="Profile Image"
           ></Image>
         </Box>
@@ -59,11 +59,45 @@ const Page = () => {
           I'm a Full-Stack programmer. I was a store manager. And love web
           design and going back to the Internet again.{" "}
         </Paragragh>
-        <Box alignItems="center" my={4}>
+        <Box textAlign="center" my={4}>
           <Button rightIcon={<CheckCircleIcon />} colorScheme="teal">
             My portfolio
           </Button>
         </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1986</BioYear>
+          Born in Taipei（台北）, Taiwan.
+        </BioSection>
+        <BioSection>
+          <BioYear>2008</BioYear>
+          Completed the Bachelor&apos;s Computer Science and Electrical
+          Engineering in the Department of Electronic Engineering at National
+          Kaosiung University of Science and
+          Technology.（國立高雄科技大學電子工程系-電腦科學與電子工程）
+        </BioSection>
+        <BioSection>
+          <BioYear>2008-2009</BioYear>
+          Military service
+        </BioSection>
+        <BioSection>
+          <BioYear>2010-2012</BioYear>
+          Front-End Developer Worked at Aesopower, Inc.（象量科技股份有限公司）
+        </BioSection>
+        <BioSection>
+          <BioYear>2014-2023</BioYear>
+          Store Manager Worked at boardgame store BgboxTaiwan（桌遊盒子）
+        </BioSection>
+      </Section>
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragragh>Music, Boardgame, Playing Guitar, Reading, Hiking</Paragragh>
       </Section>
     </Container>
   );
