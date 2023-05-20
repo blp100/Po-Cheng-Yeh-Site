78e29cef6f5fc5f59ce2c02fc7d2553e8151f073
Po-Cheng Yeh <blp100x@hotmail.com>
2023-05-20 16:25:03 +0800

    feat: add post page and resize work thumbnail

--- pages/posts.js
+++ pages/posts.js
@@ -1,7 +1,17 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Icon, Text, useColorModeValue } from "@chakra-ui/react";
+import { BsSignDeadEndFill } from "react-icons/bs";
 
 const Posts = () => {
-  return <Text textAlign="center">Git hub testing</Text>;
+  return (
+    <Box textAlign="center">
+      <Icon
+        as={BsSignDeadEndFill}
+        boxSize={36}
+        color={useColorModeValue("red.700", "#FBB6CE")}
+      />
+      <Text>Posts page is coming soon</Text>
+    </Box>
+  );
 };
 
 export default Posts;
